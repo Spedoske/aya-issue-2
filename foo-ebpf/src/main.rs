cbef0f876b0def8ef8135dcaf7f625a96e6695a5
Spedoske <leomundspedoske@gmail.com>
2023-12-19 06:38:02 -0800

    [feat] add bpf_spin_lock

--- foo-ebpf/src/main.rs
+++ foo-ebpf/src/main.rs
@@ -3,6 +3,18 @@
 
 use aya_bpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
 use aya_log_ebpf::info;
+use aya_bpf::macros::map;
+use aya_bpf::{
+    bindings::bpf_spin_lock,
+    helpers::{
+        bpf_spin_lock as bpf_spin_lock_lock,
+        bpf_spin_unlock as bpf_spin_lock_unlock,
+    },
+    maps::Array,
+};
+
+#[map]
+static Foo: Array<bpf_spin_lock> = Array::with_max_entries(1, 0);
 
 #[xdp]
 pub fn foo(ctx: XdpContext) -> u32 {
@@ -13,7 +25,11 @@ pub fn foo(ctx: XdpContext) -> u32 {
 }
 
 fn try_foo(ctx: XdpContext) -> Result<u32, u32> {
-    //info!(&ctx, "received a packet");
+    let x = unsafe {Foo.get_ptr_mut(0).ok_or(xdp_action::XDP_PASS)?.as_mut().unwrap()};
+    unsafe {
+        bpf_spin_lock_lock(x);
+        bpf_spin_lock_unlock(x);
+    }
     Ok(xdp_action::XDP_PASS)
 }
 
